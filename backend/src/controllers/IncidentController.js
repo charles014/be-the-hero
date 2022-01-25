bdbc05d04cafcc969bd35699c3ca7b92c2606e0c
charles014 <charles.bz@gmail.com>
2022-01-25 09:37:21 -0300

    Change: Remover espaços

--- backend/src/controllers/IncidentController.js
+++ backend/src/controllers/IncidentController.js
@@ -36,9 +36,7 @@ module.exports = {
 
     async delete(request, response){
         const {id} = request.params;
-        const ong_id = request.headers.authorization;
-         
-        
+        const ong_id = request.headers.authorization;                
         
         const incident = await connection('incidents')
         .where ('id', id)
